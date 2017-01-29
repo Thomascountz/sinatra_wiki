6399c30a339bf6725abfc96a130b39a6804a9c78
Thomas Countz <Thomascountz@gmail.com>
2017-01-29 00:53:23 +0000

    added new page form

--- app.rb
+++ app.rb
@@ -9,13 +9,27 @@ def page_content(title)
     return nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
 get "/" do
   erb :welcome # render './views/welcome.erb'
 end
 
+get "/new" do
+  erb :new
+end
+
 get "/:title" do
-  @title = params[:title]
+  @title = params[:title].capitalize
   @content = page_content(@title)
   erb :show
 end
 
+#{"title"=>"Title", "content"=>"This is the content of a new wiki page"}
+post "/create" do
+  params.inspect
+end
